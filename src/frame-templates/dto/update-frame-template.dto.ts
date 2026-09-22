9675ed2817b1069b76aee6b3bc3b770d423b87fc
ZeroNoSho <inukuro351@gmail.com>
2026-09-22 17:18:18 +0700

    remove frame templates module, controller, service, and DTOs

--- src/frame-templates/dto/update-frame-template.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateFrameTemplateDto } from './create-frame-template.dto';
-
-export class UpdateFrameTemplateDto extends PartialType(
-  CreateFrameTemplateDto,
-) {}
--- src/frame-templates/frame-templates.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FrameTemplatesController } from './frame-templates.controller';
-import { FrameTemplatesService } from './frame-templates.service';
-import { PrismaService } from '../prisma.service';
-@Module({
-  controllers: [FrameTemplatesController],
-  providers: [FrameTemplatesService, PrismaService],
-})
-export class FrameTemplatesModule {}
